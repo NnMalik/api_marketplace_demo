9a0a19401f38db1b12f580ff8640c2d4353d8f62
Nitin Malik <nitin.malik@nagarro.com>
2025-09-18 19:30:01 +0530

    add config changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: "/api_marketplace_demo/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
